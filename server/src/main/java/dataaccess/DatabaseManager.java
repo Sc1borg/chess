1a29a0e9b4e79641d637fc254ae6f1fdd44bbbf2
Sc1borg <ericthescienceguy@gmail.com>
2025-11-02 16:06:32 -0700

    Update DatabaseManager.java

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -46,6 +46,7 @@ public class DatabaseManager {
      */
     static Connection getConnection() throws DataAccessException {
         try {
+            createDatbase();
             //do not wrap the following line with a try-with-resources
             var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
             conn.setCatalog(databaseName);
