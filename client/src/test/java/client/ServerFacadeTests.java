c2e64d18ad4f3edcea4afdce1bdcfdb694394b5b
Sc1borg <ericthescienceguy@gmail.com>
2025-11-14 19:15:23 -0700

    I cannot get my tests to work. They just don't show up

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,26 +1,21 @@
-package java.client;
+package client;
 
-import model.LoginResult;
-import model.RegisterRequest;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.Server;
-import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
-    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        serverFacade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -28,18 +23,10 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-    @BeforeEach
-    void registerSuccess() throws Exception {
-        RegisterRequest registerRequest = new RegisterRequest("user", "pass", "email");
-        serverFacade.register(registerRequest);
-    }
-
 
     @Test
-    public void registerFailure() throws Exception {
-        RegisterRequest registerRequest = new RegisterRequest("user", "pass", "email");
-        LoginResult result = serverFacade.register(registerRequest);
-        assertTrue(obj instanceof YourClass);
+    public void sampleTest() {
+        Assertions.assertTrue(true);
     }
 
 }
